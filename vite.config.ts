4fb2bacc4ef2362785f9124b1537fadcfc2d4792
Daamleon <withdaamleon@gmail.com>
2025-03-25 12:56:35 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -16,17 +16,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "genta.png",
+            src: "genta192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "genta.png",
+            src: "genta512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "genta.png",
+            src: "genta512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
